0896e663f0d8a0aa0c60cdd2d5b844c52f042d25
Giridharan007-py <giridharanm088@gmail.com>
2025-10-23 23:32:13 +0530

    Update fibonaaci (function).py

--- FUNCTION/fibonaaci (function).py
+++ FUNCTION/fibonaaci (function).py
@@ -2,10 +2,9 @@ def fibonaaci():
     n=int(input("Enter a number:"))
     a=0
     b=1
-    i=0
-    print(a)
+    i=0
     while (i<n):
-        a,b=b,a+b
-        i+=1
         print(a)
+        a,b=b,a+b
+        i+=1
 fibonaaci()
